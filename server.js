20e431fb0745b86dd8ceb6cfe0af7fc827bbffcf
micheci <michecimartinez@gmail.com>
2022-08-03 17:36:12 -0500

    added mondoDB

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express=require('express')
+const bodyParser=require('body-parser')
 const app=express()
+const MongoClient=require('mongodb').MongoClient
+const connectionString='mongodb+srv://Micheci:Micheci12!@cluster0.6tapr9t.mongodb.net/?retryWrites=true&w=majority'
+
+
+MongoClient.connect(connectionString,(err,client)=>{
+    if (err)return console.error(err)
+    console.log("connected to database")
+})
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.listen(3000,(req,res)=>{
     console.log("hi")
 })
 app.get('/',(req,res)=>{
-    res.send("Hello world")
+    res.sendFile(__dirname+'/index.html')
 }
 )
+app.post('/quotes',(req,res)=>{
+console.log(req.body)
+})
